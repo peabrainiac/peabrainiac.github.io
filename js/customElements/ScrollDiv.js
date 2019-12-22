f8873288c09f6623c760fcb62be6f2db951fc66f
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2019-12-23 00:04:57 +0100

    added horizontal scrollbar

--- js/customElements/ScrollDiv.js
+++ js/customElements/ScrollDiv.js
@@ -36,25 +36,39 @@ export default class ScrollDiv extends HTMLElement {
                     min-height: 100%;
                     position: relative;
                 }
-                #scrollbar {
+                #scrollbar-h, #scrollbar-v {
+                    background: #101010;
+                    opacity: 1;
+                }
+                #scrollbar-h {
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    right: 0;
+                    margin: 0 20px 5px 10px;
+                }
+                #scrollbar-v {
                     position: absolute;
                     top: 0;
                     right: 0;
                     bottom: 0;
                     margin: 10px 5px 20px 0;
-                    background: #101010;
-                    opacity: 1;
                 }
-                #scrollbar, #scrollbar-handle {
+                #scrollbar-h, #scrollbar-handle-h {
+                    height: 5px;
+                }
+                #scrollbar-v, #scrollbar-handle-v {
                     width: 5px;
+                }
+                #scrollbar-h, #scrollbar-v, #scrollbar-handle-h, #crollbar-handle-v {
                     border-radius: 3px;
                     transition: all 0.25s ease-out;
                 }
-                #scrollbar-handle {
+                #scrollbar-handle-h, #scrollbar-handle-v {
                     position: absolute;
                     background: #303030;
                 }
-                #scrollbar.hidden {
+                #scrollbar-h.hidden, #scrollbar-v.hidden {
                     opacity: 0;
                 }
             </style>
@@ -63,20 +77,30 @@ export default class ScrollDiv extends HTMLElement {
                     <slot></slot>
                 </div>
             </div>
-            <div id="scrollbar" class="hidden">
-                <div id="scrollbar-handle"></div>
+            <div id="scrollbar-h" class="hidden">
+                <div id="scrollbar-handle-h"></div>
+            </div>
+            <div id="scrollbar-v" class="hidden">
+                <div id="scrollbar-handle-v"></div>
             </div>
         `;
         var container = shadowRoot.getElementById("outer-container");
-        var scrollbar = shadowRoot.getElementById("scrollbar");
-        var handle = shadowRoot.getElementById("scrollbar-handle");
+        var horizontalScrollbar = shadowRoot.getElementById("scrollbar-h");
+        var verticalScrollbar = shadowRoot.getElementById("scrollbar-v");
+        var horizontalHandle = shadowRoot.getElementById("scrollbar-handle-h");
+        var verticalHandle = shadowRoot.getElementById("scrollbar-handle-v");
 
         function update(){
+            var contentWidth = container.scrollWidth;
             var contentHeight = container.scrollHeight;
+            var width = container.offsetWidth;
             var height = container.offsetHeight;
-            scrollbar.classList.toggle("hidden",contentHeight<=height+1);
-		    handle.style.top = container.scrollTop*scrollbar.offsetHeight/contentHeight+"px";
-            handle.style.bottom = (contentHeight-height-container.scrollTop)*scrollbar.offsetHeight/contentHeight+"px";
+            horizontalScrollbar.classList.toggle("hidden",contentWidth<=width+1);
+            verticalScrollbar.classList.toggle("hidden",contentHeight<=height+1);
+		    horizontalHandle.style.left = container.scrollLeft*horizontalScrollbar.offsetWidth/contentWidth+"px";
+		    verticalHandle.style.top = container.scrollTop*verticalScrollbar.offsetHeight/contentHeight+"px";
+            horizontalHandle.style.right = (contentWidth-width-container.scrollLeft)*horizontalScrollbar.offsetWidth/contentWidth+"px";
+            verticalHandle.style.bottom = (contentHeight-height-container.scrollTop)*verticalScrollbar.offsetHeight/contentHeight+"px";
         }
 
         container.addEventListener("scroll",update);
