35280db66fac9747aea15e10239fdf02b4af4fd9
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2020-04-01 22:14:16 +0200

    added custom text input

---
+++ js/customElements/TextInput.js
@@ -0,0 +1,77 @@
+export default class TextInput extends HTMLElement {
+	constructor(){
+		super();
+		this.attachShadow({mode:"open"});
+		this.shadowRoot.innerHTML = `
+			<style>
+				:host {
+					display: inline-block;
+					position: relative;
+					min-width: 185px;
+					padding: 1px 3px;
+					border: 1px solid transparent;
+					transition: border-bottom-color 0.25s ease;
+					font-family: "MS Shell Dlg 2", sans-serif; 
+				}
+				input {
+					color: inherit;
+					background: transparent;
+					position: absolute;
+					left: 0;
+					top: 0;
+					width: 100%;
+					height: 100%;
+				}
+				span {
+					visibility: hidden;
+				}
+				input, span {
+					padding: 0;
+					border: none;
+					font: inherit;
+					text-align: inherit;
+					white-space: pre;
+				}
+			</style>
+			<span> </span>
+			<input type="text" class="inner-input">
+		`;
+		this._input = this.shadowRoot.querySelector("input");
+		this._text = this.shadowRoot.querySelector("span");
+		this._input.addEventListener("input",()=>{
+			this._text.innerText = this._input.value||" ";
+		});
+	}
+
+	focus(){
+		this._input.focus();
+	}
+
+	get value(){
+		return this._input.value;
+	}
+
+	set value(value){
+		this._input.value = value;
+		this._text.innerText = value||" ";
+	}
+
+	static get observedAttributes(){
+		return ["placeholder"];
+	}
+
+	attributeChangedCallback(name,oldValue,newValue){
+		if (name=="placeholder"){
+			this._input.setAttribute(name,newValue);
+		}
+	}
+
+	get placeholder(){
+		return this.getAttribute("placeholder");
+	}
+
+	set placeholder(value){
+		this.setAttribute("placeholder",value);
+	}
+}
+customElements.define("text-input",TextInput);
